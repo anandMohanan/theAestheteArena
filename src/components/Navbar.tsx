b0ebf2ba6f1f45659a5615f896f480f55bc2d8a3
anand <36781763+anandMohanan@users.noreply.github.com>
2023-09-23 10:44:29 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = async () => {
   const session = await getServerSession();
 
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-primary-colour  z-[10] py-2">
+    <div className="top-0 inset-x-0 h-fit bg-primary-colour  z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center lg:w-60">
           <Image src="/logo.png" alt="logo" width={40} height={40} />
